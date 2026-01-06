963acebe1cbf1ff8d50a32b900b3406c2cebecba
Rajat Singh Rajawat <rajatsinghrajawatrajawat@gmail.com>
2026-01-06 11:49:20 +0530

    pu

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 console.log("hello world")
+
+
+// var add = 45
+// add=46
+
+
+// // var add = 54
+// console.log(add)
+
+
+
+// let add = 45
+
+// // let add =45
+
+
+// add = 54
+// console.log(add);
+
+
+
+
+const add = 455
+ add = 4556
+
+console.log(add);
