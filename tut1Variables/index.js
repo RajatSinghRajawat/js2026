7aa61a48aaaa3ff874d9d0772834fda68c9cb61e
Rajat Singh Rajawat <rajatsinghrajawatrajawat@gmail.com>
2026-01-13 10:04:48 +0530

    PUSH

--- tut1Variables/index.js
+++ tut1Variables/index.js
@@ -24,3 +24,122 @@ console.log("hello world")
 
 // console.log(add);
 
+// ********************************************* Declaration and Initialization *********************
+// var x; ==> declaration
+// x = 10; ==> Initialization / assign
+
+// ***********************************************  variable using var keyword
+// 1. var variable is global and function scope;
+// var  a= 10;
+
+// {
+//     var b = 20;
+// }
+// console.log(b)
+
+// 2. we can do re-declare and reassigned with var variable
+
+// ***********************************************  variable using let keyword
+// 1. we can not re-declare let variable
+// let a =10;
+// let a = 20;
+
+// 2. we can reassigned
+// let a = 10;
+// a = 20;
+
+// 3. let variable is block level scope;
+// {
+//     let mylet = 30;
+// }
+
+// console.log(mylet)
+
+// ***********************************************  variable using const keyword
+// 1. we can not re-declare and reassigned const variable
+// 2. this is block level scope
+
+// *******************************************************
+// var firstName = "Ravi";
+// let lastName = "Singh";
+// let age = 15;
+// firstName = 100;
+// var a;
+
+// const hoursInDay = 24;
+
+// let userIntro =
+//   "Hi, my name is " +
+//   firstName +
+//   " " +
+//   lastName +
+//   ". I am " +
+//   age +
+//   " " +
+//   "years old.";
+
+// ***************************************************
+// console.log(a);
+// var a = 10;
+
+// console.log(b);
+// let b = 10;
+
+// *************** we can not give varible name lke this
+// let last*Name = "Singh";
+// let last%Name = "Singh";
+// let last^Name = "Singh";
+// let 2lastName = "Singh";
+
+// ** we can make varaible name like this
+// let last$Name = "Singh";
+// let last_Name = "Singh";
+// let $lastName = "Singh";
+// let _lastName = "Singh";
+// let last2Name = "Singh";
+// let lastName2 = "Singh";
+
+// *************** we can also write this but this is not right
+// let खुशहै = true;
+// console.log(खुशहै);
+
+// https://www.freecodecamp.org/news/programming-naming-conventions-explained
+
+/*
+
+
+
+Naming Conventions -->
+developer ko जब कोड मे नाम देना होता है तो वह different case type use करते हैं नाम देने के लिए
+
+naming conventions एक तरीके का रूल होता है जो हमे हेल्प करता है जब हमे variable ko नाम देना होता है जिससे हमे कोड को पढ़ने, मेंटेन और समझ ने मे मदद मिलती है, जिसमे से चार फेमस है
+
+1.Camel Case
+2.Snake Case
+3.Kebab Case
+4.Pascal Case
+
+
+1.What is Camel Case?
+In camel case, you start a name with a small letter. If the name has multiple words, the later words will start with a capital letter:
+
+Here are some examples of camel case: firstName and lastName.
+
+
+2.What is Snake Case?
+Like in camel case, you start the name with a small letter in snake case. If the name has multiple words, the later words will start with small letters and you use a underscore (_) to separate the words.
+
+Here are some examples of snake case: first_name and last_name.
+
+
+3.Kebab case is similar to snake case, but you use a hyphen (-) instead of an underscore (_) to separate the words.
+
+Here are some examples of kebab case: first-name and last-name.
+
+4.What is Pascal Case?
+Unlike the previous examples, names in pascal case start with a capital letter. In case of the names with multiple words, all words will start with capital letters.
+
+Here are some examples of pascal case: FirstName and LastName.
+
+
+*/
---
+++ tut2Datatypes/index.js
@@ -0,0 +1,69 @@
+// Data Types
+
+// Strings 
+
+// let strings = " helllo"
+// console.log(strings);
+// alert(strings)
+
+
+
+// console.log(+"100")
+// alert(+"errdljfkdkfj")
+
+
+
+// console.log(parseInt("10000"))
+// console.log(parseInt("10000sdiugysuoifhuih"))
+// console.log(parseInt("sdiugysuoifhuih"))
+  
+
+// integer  Number Float
+
+
+
+// let number = 45
+
+// alert(number)
+
+
+
+// null // undefined 
+
+// let a  = null 
+// let b;
+
+
+// bigint 
+
+// let bignumber  = 5486656489654896457896458964758458n;
+// let bignumber2  = BigInt(5486656489654896457896458964758458);
+// console.log(bignumber);
+// console.log(bignumber2);
+
+
+// symbol  
+
+// symbol unique value  
+
+// let a  = Symbol("1234")
+// let b  = Symbol("1234")
+// console.log(a);
+// console.log(b);
+
+// console.log(a==b);
+
+
+
+// Boolean 
+
+// true false 
+// console.log(+true)
+// console.log(+false)
+
+
+
+
+// typeof 
+// let a
+// console.log(typeof(undefined))
