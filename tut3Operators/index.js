652ad1b0d1dfe840e952304b5e38d2d68fb150ea
Rajat Singh Rajawat <rajatsinghrajawatrajawat@gmail.com>
2026-01-13 11:16:50 +0530

    tut4

---
+++ tut3Operators/index.js
@@ -0,0 +1,44 @@
+let a = 10;
+let b = 10
+let c = a + b
+console.log(c);
+
+
+let sub1 = 45
+let sub2 = 45
+let sub3 = sub1 - sub2
+
+console.log(sub3);
+
+
+// divide
+
+
+let div1 = 42;
+let div2 = 2
+let div3 = div1 / div2
+console.log(div3);
+
+
+let mul1 = 12
+let mult2 = 12
+let mul3 = mul1 * mult2
+console.log(mul3);
+
+
+let square = 2
+console.log(square ** 2);
+
+
+
+// let user = prompt("enter your numer")
+// console.log(user);
+
+
+let confirms = confirm("Are you 18+")
+if (confirms) {
+   alert("yes")
+
+} else {
+   alert("no")
+}
