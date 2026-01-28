cc503df10b908a472a4deaefbf3604e8b2c667d7
Rajat Singh Rajawat <rajatsinghrajawatrajawat@gmail.com>
2026-01-28 09:24:44 +0530

    push

---
+++ tut10stringsmethods/index.js
@@ -0,0 +1,25 @@
+// let start = prompt("enter yoru name ")
+
+// console.log( start.length);
+// let text = "Hello World";
+// console.log(text.includes("World"));
+
+
+// let text = "Hello World";
+// console.log(text.indexOf("World"));
+
+// let text = "I love JS";
+// console.log(text.replace(" ", "JavaScript"));
+
+// let a = "Hello";
+// let b = " World";
+// console.log(a.concat(b));
+
+// let num = "7";
+// console.log(num.padStart(3, "0"));
+
+// let text = "Hello";
+// console.log(text.charAt(1));
+
+let text = "Hello World JS";
+console.log(text.split(" "));
--- tut9SwitchCase/index.js
+++ tut9SwitchCase/index.js
@@ -1,31 +1,64 @@
-let daynumber = parseInt(prompt("daynumbeer"))
+// let daynumber = parseInt(prompt("daynumbeer"))
 
 
-switch (daynumber) {
-    case 1:
-        console.log("sunday")
-        break;
-    case 2:
-        console.log("sunday2")
-        break;
+// switch (daynumber) {
+//     case 1:
+//         console.log("sunday")
+//         break;
+//     case 2:
+//         console.log("sunday2")
+//         break;
 
-    case 3:
-        console.log("sunday3")
-        break;
+//     case 3:
+//         console.log("sunday3")
+//         break;
 
-    case 4:
-        console.log("sunday4")
-        break;
+//     case 4:
+//         console.log("sunday4")
+//         break;
 
-    case 5:
-        console.log("sunday5")
-        break;
+//     case 5:
+//         console.log("sunday5")
+//         break;
 
-    case 6:
-        console.log("sunday6")
+//     case 6:
+//         console.log("sunday6")
+//         break;
+
+//     default:
+//         console.log("no")
+
+// }
+
+
+
+// console.log( !! 0);
+
+
+
+let username = prompt("enter your name  ")
+let userAge = parseInt(prompt("enter your age  "))
+
+
+
+switch (true) {
+    case userAge >= 24 && userAge <= 30: {
+        console.log(`${username} age is enable to going to visit jaipur tour `)
+        alert(`${username} Yehhh ! You are eligible for jaipur tour Enjoy.`)
+
         break;
+    }
+
+    case userAge < 24 && userAge >= 18: {
+        console.log(`${username} age is enable to going to visit jaipur tour `)
+        alert(`${username} Yehhh ! You are eligible for jaipur tour Enjoy.`)
+
+        break;
+    }
 
     default:
-        console.log("no")
+        console.log("sorry ! age is not eligible for jaipur tour ");
+        alert("sorry !this age is not eligible for jaipur tour try again")
 
+        break;
 }
