cd5439dfe7d54b2d7bcaad8444fd761f7b474904
Rajat Singh Rajawat <rajatsinghrajawatrajawat@gmail.com>
2026-01-29 11:31:52 +0530

    push

--- tut10stringsmethods/index.js
+++ tut10stringsmethods/index.js
@@ -1,36 +1,61 @@
-// let userName = ""
+// // let userName = ""
 
 
 
 
-// console.log(userName.length)
-// console.log(userName.toUpperCase());
-// console.log(userName.toLowerCase());
-// console.log(userName.trim());
-// console.log(userName.trimStart());
+// // console.log(userName.length)
+// // console.log(userName.toUpperCase());
+// // console.log(userName.toLowerCase());
+// // console.log(userName.trim());
+// // console.log(userName.trimStart());
 
-// // console.log(userName.includes());
-
-
-
-let a = "i am gaurav"
-
-
-// console.log(a.concat(b));
-console.log(a.replace("gaurav", "love"));
-
-
-let b = " hellloo hii"
-console.log(b.includes("hii"));
+// // // console.log(userName.includes());
 
 
 
-let text = "Hello World";
-console.log(text.split(" "));
+// let a = "i am gaurav"
+
+
+// // console.log(a.concat(b));
+// console.log(a.replace("gaurav", "love"));
+
+
+// let b = " hellloo hii"
+// console.log(b.includes("hii"));
+
+
+
+// let text = "Hello World";
+// console.log(text.split(" "));
 
 
 
 
-// let k = "sdds "
+// // let k = "sdds "
 
-// console.log(k.padStart("3" , 0));
+// // console.log(k.padStart("3" , 0));
+
+
+const grade = "1";
+
+switch (grade.toLowerCase()) {
+  case "a":
+    console.log("Your score is between 85% to 100%.");
+    break;
+  case 1:
+    console.log("Your score is between 75% to 85%");
+    break;
+  case "c":
+    console.log("Your score is between 60% to 75%");
+    break;
+  case false:
+    console.log("Your score is between 50% to 60%");
+    break;
+  case "e":
+    console.log("Your score is between 30% to 50%");
+    break;
+  default:
+    console.log("Sorry, you failed.");
+}
+
+console.log("Program Ended!");
