51d35044024b87828144e91e8624ecdadd33e378
Rajat Singh Rajawat <rajatsinghrajawatrajawat@gmail.com>
2026-02-04 14:25:54 +0530

    push

---
+++ tut11Object/index.js
@@ -0,0 +1,100 @@
+
+
+
+// // let studentData = {
+
+// //   username: "love",
+// //   age: 20,
+// //   mob: 7896547896,
+
+// // }
+
+
+// // console.log(studentData.username);
+// // console.log(studentData.age);   //dot notations 
+
+// // console.log(studentData["username"]); //squere 
+
+
+
+// // let obj = {
+// //   user: "hello",
+// //   a: {
+// //     name: "abc",
+// //     class: 5,
+// //     b: {
+// //       add: "mains"
+// //     }
+// //   }
+// // }
+
+
+// // console.log(obj.a.name);
+
+
+
+
+
+// // if u want to update your keys value 
+
+
+
+// let studentdata = {
+//   "name": "love rajput",
+//   age: 20,
+//   mob: 784544125485,
+//   address: {
+//     city: "alwar",
+//     state: "rajasthan",
+//     pin: 301001
+//   }
+// }
+
+
+// console.log("before", studentdata);
+
+
+// studentdata.address.city = "udaipur" /// to replace our keys valiue
+
+
+// console.log("after", studentdata);
+
+
+// studentdata.place = "udaipur"
+
+// console.log("after add", studentdata); // addd new key ib our objet
+
+
+// delete studentdata.place
+
+// console.log(studentdata);
+
+
+
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 30,
+//   eyeColor: "blue"
+// };
+
+// console.log("agkje" in person);
+// console.log(person.hasOwnProperty("firstName"));
+
+// console.log(person.firstName);
+// person.age = 35;
+
+// person.country = "USA";
+
+
+// Object.freeze(person);
+
+// person.age = 32;
+// person.country = "USA";
+
+
+// console.log(person); 
+
+// Object.seal(person);
+// person.groom = true;
+// console.log(person);
---
+++ tut12Array/index.js
@@ -0,0 +1,58 @@
+let arr = [1,2,3,4,5]
+
+// console.log(arr.length);
+
+
+//// first method to add value in last 
+// arr[5] = 6
+
+// console.log(arr);
+
+
+// second method to add value in last 
+
+// arr[arr.length] = 6
+
+// console.log(arr);
+
+
+// third method for add value in a;lst arr //push 
+
+// arr.push(6)
+
+// console.log(arr);
+
+
+/// pop /// remove value in first in aarray
+
+
+
+// arr.pop()
+
+// console.log(arr);
+
+
+
+
+// shift remove first value in our array
+
+
+arr.shift()
+console.log(arr);
+
+
+
+// unshift add valiue inn fist in array
+
+
+// arr.unshift(1)
+// console.log(arr);
+
+
+// console.log(arr.join("*"));
+
+
+const user = "45"
+console.log(typeof user);
+
+console.log(typeof parseInt(user));
