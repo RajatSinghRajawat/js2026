f7d0c74b730befdbfac66fd9dfa372910a7afe86
Rajat Singh Rajawat <rajatsinghrajawatrajawat@gmail.com>
2026-02-17 09:46:15 +0530

    push

--- tut13CopyArray&Object/index.js
+++ tut13CopyArray&Object/index.js
@@ -39,3 +39,52 @@
 // b=c++;
 // console.log(a);
 // console.log(b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let arr1  = [1,2,3]
+// let arr2  = [1,2,3]
+// let arr3  = [...arr1 , ...arr2]
+// console.log(arr3);
+
+
+// const arr1 = ["a", "b", "c"];
+// const arr2 = Array.from(arr1);
+// console.log(arr2);
+
+
+// const arr1 = ["a", "b", "c"];
+// let arr2  = arr1.slice(1,3)
+// console.log(arr2);
+
+// const arr =  [1,2,3,4,5,6]
+// console.log(arr.splice(1,3));
+
+// ...spread operator  valuees store 
+
+
+
+
+const obj = {
+    name:"raj"
+}
+
+
+const obj2 = Object.assign({} , obj)
+console.log(obj2);
---
+++ tut14ScopeVariables/index.js
@@ -0,0 +1,38 @@
+// // let x = 1
+
+
+
+// // {
+// //     let x = 2;
+// //     console.log("bloacks x", x);
+
+// // }
+2
+// // console.log("global x", x);
+
+// {
+
+//     let a = 5
+//     const b = 5
+
+//     var c = 5
+
+
+//     console.log("block : ",a);
+//     console.log("block : ",c);
+//     console.log("block : ",b);
+
+// }
+
+// console.log(a);
+// console.log(c);
+// console.log(b);
+
+// let a = 5
+// a*=3+
+// console.log(a);
+
+
+let a = 4;
+a *= a++ * ++a;
+console.log(a);
