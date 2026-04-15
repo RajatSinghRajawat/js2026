7a3f9504b60d95a6b82cd0844f870a883dbdca4a
Rajat Singh Rajawat <rajatsinghrajawatrajawat@gmail.com>
2026-04-15 11:27:42 +0530

    push

--- tut19clousers/index.js
+++ tut19clousers/index.js
@@ -1,28 +1,113 @@
-function outer() {
-  let count = 0;
+// function outer() {
+//   let count = 0;
 
-  function inner() {
-    count++;
-    console.log(count);
-  }
+//   function inner() {
+//     count++;
+//     console.log(count);
+//   }
 
-  return inner;
-}
+//   return inner;
+// }
 
-const myFunc = outer();
+// const myFunc = outer();
 
-myFunc(); // 1
-myFunc(); // 2
-myFunc(); // 3
+// myFunc(); // 1
+// myFunc(); // 2
+// myFunc(); // 3
 
 
-function greet(name) {
-  return function(message) {
-    console.log(`${message}, ${name}`);
-  };
-}
+// function greet(name) {
+//   return function(message) {
+//     console.log(`${message}, ${name}`);
+//   };
+// }
 
-const sayHello = greet("Rajat");
+// const sayHello = greet("Rajat");
 
-sayHello("Hello"); // Hello, Rajat
-sayHello("Hi");    // Hi, Rajat
+// sayHello("Hello"); // Hello, Rajat
+// sayHello("Hi");    // Hi, Rajat
+
+
+
+
+
+
+
+
+
+
+// function fnc() {
+
+//   let count = 0;
+
+//   function inner() {
+//     count++
+//     console.log(count);
+//   }
+//   return inner
+
+
+// }
+
+
+
+// const innerfnc = fnc()
+// innerfnc()
+// innerfnc()
+
+
+
+
+
+
+
+
+
+
+// function outer(name) {
+
+//   function inner(message) {
+//     console.log(`${name} ${message}`);
+
+//   }
+//   return inner
+
+// }
+
+
+// const a = outer("dhurv")
+
+// a("helloo")
+// a("hii")
+// a("helloo")
+// a("helloo")
+// a("helloo")
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
